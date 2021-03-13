dbe2a582fa0d46202320b1684f8e628db70b0613
Oliver Otchere <oliverotchere4@gmail.com>
2021-03-13 05:08:02 +0000

    added models and view

--- api/models.py
+++ api/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from random import choices
+from string import ascii_letters
+from django.conf import settings
 
-# Create your models here.
+class Link(models.Model):
+    original_link=models.URLField()
+    shortened_link=models.URLField(blank=True,null=True)
+
+
+    def shortener(self):
+        while True:
+            random_string=''.join(choices(ascii_letters,k=6))
+            new_link=settings.HOST_URL+'/'+random_string
+    
+            if not Link.objects.filter(shortened_link=new_link).exists():
+                break
+
+        return new_link
+
+    def save(self,*args, **kwargs):
+        if not self.shortened_link:
+            new_link=self.shortener()
+            self.shortened_link=new_link
+        return super().save(*args, **kwargs)
+
+
--- api/views.py
+++ api/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from rest_framework.generics import ListAPIView,CreateAPIView
 
-# Create your views here.
+from django.views import View
+from django.conf import settings
+
+from .models import Link
+from .serializer import LinkSerializer
+
+class ShortenerListAPIView(ListAPIView):
+    queryset=Link.objects.all()
+    serializer_class=LinkSerializer
+
+class ShortenerCreateApiView(CreateAPIView):
+    serializer_class=LinkSerializer
+
+
+class Redirector(View):
+    def get(self,request,shortener_link,*args, **kwargs):
+        shortener_link=settings.HOST_URL+'/'+self.kwargs['shortener_link']
+        redirect_link=Link.objects.filter(shortened_link=shortener_link).first().original_link
+        return redirect(redirect_link)
